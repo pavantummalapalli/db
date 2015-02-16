cced5770f6ed5b497e5ed24d20be8e9f24b588da
Chandra Kant Sharma <cksharma.skt@gmail.com>
2015-02-15 20:22:41 -0500

    main file changes

--- src/edu/buffalo/cse562/Main.java
+++ src/edu/buffalo/cse562/Main.java
@@ -2,29 +2,28 @@ package edu.buffalo.cse562;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.print("We, the members of our team, agree that we will not submit any code that we have not written ourselves, share our code with anyone outside of our group, or use code that we have not written ourselves as a reference.");
-		if(args.length==0){
+		if (args.length < 3) {
 			printUsage();
 			return;
 		}
-		int i=0;
-		for(;i<args.length ;i++)
-			if(args[i].equalsIgnoreCase("--data"))
-				break;
-		if(i+2>=args.length){
-			printUsage();
-			return;
+		
+		String dataDir = "";
+		String sqlFiles[] = new String[args.length - 2];
+		int index = 0;
+		boolean flag = true;
+		for (int i = 0; i < args.length; i++) {
+			if (flag && args[i].equals("--data")) {
+				dataDir = args[i + 1];
+				flag = false;			
+				i++;
+			} else {
+				sqlFiles[index++] = args[i];
+			}
 		}
-		String dataDir = args[++i];
-		String sqlFiles[]= new String[args.length-i-1];
-		int count=0;
-		while(++i<args.length){
-			sqlFiles[count++]=args[i];
-		}
-		new StatementReader().readSqlFile(dataDir,sqlFiles);
+		new StatementReader().readSqlFile(dataDir, sqlFiles);
 	}
-	
-	private static void printUsage(){
+
+	private static void printUsage() {
 		System.out.println("Enter data directory.\n Usage --data Data_Dir SQLFile1.sql [SQLFile2.sql] [SQLFile3.sql]");
 	}
 }
