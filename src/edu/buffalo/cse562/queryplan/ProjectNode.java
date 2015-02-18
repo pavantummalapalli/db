a8a2776cb6639123775b2cad3e3c226b0faa87e8
Chandra Kant Sharma <cksharma.skt@gmail.com>
2015-02-18 07:44:43 -0500

    project node changes

--- src/edu/buffalo/cse562/queryplan/ProjectNode.java
+++ src/edu/buffalo/cse562/queryplan/ProjectNode.java
@@ -3,9 +3,12 @@ package edu.buffalo.cse562.queryplan;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import net.sf.jsqlparser.expression.Function;
+import net.sf.jsqlparser.statement.create.table.ColumnDefinition;
 
 public class ProjectNode implements Node {
 
@@ -61,14 +64,36 @@ public class ProjectNode implements Node {
 	@Override
 	public RelationNode eval() {
 		RelationNode relationNode = childNode.eval();
+		
 		try {
 			//FileReader fileReader = new FileReader(TableUtils.getDataDir() + File.separator + tableName + ".dat");
+			//R.B, R.A
+			//check for column size and function list size
 			FileReader fileReader = new FileReader(relationNode.getFilePath());
 			BufferedReader bufferedReader = new BufferedReader(fileReader);
 			String rowVal;
-			while((rowVal = bufferedReader.readLine()) != null) {
-				System.out.println(rowVal);
+			List <ColumnDefinition> columnDefList = relationNode.getSchema().getColumnDefinitions();
+			Map <String, Integer> columnIndexMap = new HashMap <>();
+			int cnt = 0;
+			for (ColumnDefinition columnDef : columnDefList) {
+				columnIndexMap.put(columnDef.getColumnName(), cnt++);
 			}
+			if (columnList != null && !columnList.isEmpty()) {				
+				//TODO later bind System.out with PrintWriter and show output.
+				StringBuilder sb = new StringBuilder();
+				while((rowVal = bufferedReader.readLine()) != null) {
+					if (rowVal.trim().isEmpty()) continue;	
+					String[] colVals = rowVal.split("\\|");
+					for (String column : columnList) {
+						sb.append(columnIndexMap.get(column) + "|");
+					}
+					if (sb.length() > 0) {
+						System.out.println(sb.substring(0, sb.length() - 1).toString());
+					}
+				}
+			} else if (functionList != null && !functionList.isEmpty()){
+				
+			}	
 			bufferedReader.close();
 			fileReader.close();
 		} catch (IOException e) {
