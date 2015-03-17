f21483c10f6bd159b2d3ec45d5c2632e0413c6d4
Chandra Kant Sharma <cksharma.skt@gmail.com>
2015-03-17 01:27:22 -0400

    added 5 more sql

--- tpch/query/query10.sql
+++ tpch/query/query10.sql
@@ -1,4 +1,51 @@
-INCLUDE 'examples/queries/tpch/schemas.sql';
+CREATE TABLE LINEITEM (
+         orderkey       INT,
+        partkey        INT,
+        suppkey        INT,
+        linenumber     INT,
+        quantity       DECIMAL,
+        extendedprice  DECIMAL,
+        discount       DECIMAL,
+        tax            DECIMAL,
+        returnflag     CHAR(1),
+        linestatus     CHAR(1),
+        shipdate       DATE,
+        commitdate     DATE,
+        receiptdate    DATE,
+        shipinstruct   CHAR(25),
+        shipmode       CHAR(10),
+        comment        VARCHAR(44)
+);
+
+CREATE TABLE ORDERS (
+        orderkey      INT,
+        custkey        INT,
+        orderstatus    CHAR(1),
+        totalprice     DECIMAL,
+        orderdate      DATE,
+        orderpriority  CHAR(15),
+        clerk          CHAR(15),
+        shippriority   INT,
+        comment        VARCHAR(79)
+);
+
+CREATE TABLE CUSTOMER (
+        custkey      INT,
+        name         VARCHAR(25),
+        address      VARCHAR(40),
+        nationkey    INT,
+        phone        CHAR(15),
+        acctbal      DECIMAL,
+        mktsegment   CHAR(10),
+        comment      VARCHAR(117)
+);
+
+CREATE TABLE NATION (
+        nationkey    INT,
+        name         CHAR(25),
+        regionkey    INT,
+        comment      VARCHAR(152)
+);
 
 SELECT  c.custkey, c.name, 
         c.acctbal,
--- tpch/query/query5.sql
+++ tpch/query/query5.sql
@@ -1,4 +1,67 @@
-INCLUDE 'examples/queries/tpch/schemas.sql';
+CREATE TABLE LINEITEM (
+         orderkey       INT,
+        partkey        INT,
+        suppkey        INT,
+        linenumber     INT,
+        quantity       DECIMAL,
+        extendedprice  DECIMAL,
+        discount       DECIMAL,
+        tax            DECIMAL,
+        returnflag     CHAR(1),
+        linestatus     CHAR(1),
+        shipdate       DATE,
+        commitdate     DATE,
+        receiptdate    DATE,
+        shipinstruct   CHAR(25),
+        shipmode       CHAR(10),
+        comment        VARCHAR(44)
+);
+
+CREATE TABLE ORDERS (
+        orderkey      INT,
+        custkey        INT,
+        orderstatus    CHAR(1),
+        totalprice     DECIMAL,
+        orderdate      DATE,
+        orderpriority  CHAR(15),
+        clerk          CHAR(15),
+        shippriority   INT,
+        comment        VARCHAR(79)
+);
+
+CREATE TABLE CUSTOMER (
+        custkey      INT,
+        name         VARCHAR(25),
+        address      VARCHAR(40),
+        nationkey    INT,
+        phone        CHAR(15),
+        acctbal      DECIMAL,
+        mktsegment   CHAR(10),
+        comment      VARCHAR(117)
+);
+
+CREATE TABLE SUPPLIER (
+        suppkey      INT,
+        name         CHAR(25),
+        address      VARCHAR(40),
+        nationkey    INT,
+        phone        CHAR(15),
+        acctbal      DECIMAL,
+        comment      VARCHAR(101)
+);
+
+CREATE TABLE NATION (
+        nationkey    INT,
+        name         CHAR(25),
+        regionkey    INT,
+        comment      VARCHAR(152)
+);
+  
+CREATE TABLE REGION (
+        regionkey    INT,
+        name         CHAR(25),
+        comment      VARCHAR(152)
+);
 
 SELECT n.name, SUM(l.extendedprice * (1 - l.discount)) AS revenue 
 FROM   customer c, orders o, lineitem l, supplier s, nation n, region r
@@ -10,5 +73,5 @@ WHERE  c.custkey = o.custkey
   AND  n.regionkey = r.regionkey 
   AND  r.name = 'ASIA'
   AND  o.orderdate >= DATE('1994-01-01')
-  AND  o.orderdate <  DATE('1994-01-01') + interval '1' year 
+  AND  o.orderdate <  DATE('1995-01-01')
 GROUP BY n.name
