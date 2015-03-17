91231192864c2130f1f04cc0dcf0c56d08e6391a
Chandra Kant Sharma <cksharma.skt@gmail.com>
2015-03-17 01:16:49 -0400

    added al tpch queries

---
+++ tpch/query/query1.sql
@@ -0,0 +1,41 @@
+--INCLUDE 'examples/queries/tpch/schemas.sql';
+
+CREATE TABLE LINEITEM (
+       	orderkey       INT,
+        partkey        INT,
+        suppkey        INT,
+        linenumber     INT,
+        quantity       DECIMAL,
+        extendedprice  DECIMAL,
+        discount       DECIMAL,
+        tax            DECIMAL,
+        returnflag     CHAR(1),
+        linestatus     CHAR(1),
+        shipdate       DATE,
+        commitdate     DATE,
+        receiptdate    DATE,
+        shipinstruct   CHAR(25),
+        shipmode       CHAR(10),
+        comment        VARCHAR(44)
+);
+
+-- SELECT returnflag, linestatus, 
+--   SUM(quantity) AS sum_qty,
+--   SUM(extendedprice) AS sum_base_price,
+--   SUM(extendedprice * (1-discount)) AS sum_disc_price,
+--   SUM(extendedprice * (1-discount)*(1+tax)) AS sum_charge,
+--   AVG(quantity) AS avg_qty,
+--   AVG(extendedprice) AS avg_price,
+--   AVG(discount) AS avg_disc,
+--   COUNT(*) AS count_order
+-- FROM lineitem WHERE shipdate <= DATE('1997-09-01')
+-- GROUP BY returnflag, linestatus; 
+
+SELECT returnflag, linestatus, sum(quantity) AS sum_qty,
+sum(extendedprice) AS sum_base_price, 
+sum(extendedprice * (1 - discount)) 
+AS sum_disc_price, sum(extendedprice * (1 - discount) * (1 + tax)) AS sum_charge, 
+avg(quantity) AS avg_qty, avg(extendedprice) AS avg_price, 
+avg(discount) AS avg_disc, count(*) AS count_order 
+FROM lineitem WHERE shipdate <= DATE('1998-09-01') 
+GROUP BY returnflag, linestatus ORDER BY returnflag, linestatus;
---
+++ tpch/query/query3.sql
@@ -0,0 +1,64 @@
+--INCLUDE 'examples/queries/tpch/schemas.sql';
+
+CREATE TABLE LINEITEM (
+         orderkey       INT,
+        partkey        INT,
+        suppkey        INT,
+        linenumber     INT,
+        quantity       DECIMAL,
+        extendedprice  DECIMAL,
+        discount       DECIMAL,
+        tax            DECIMAL,
+        returnflag     CHAR(1),
+        linestatus     CHAR(1),
+        shipdate       DATE,
+        commitdate     DATE,
+        receiptdate    DATE,
+        shipinstruct   CHAR(25),
+        shipmode       CHAR(10),
+        comment        VARCHAR(44)
+);
+
+CREATE TABLE ORDERS (
+        orderkey      INT,
+        custkey        INT,
+        orderstatus    CHAR(1),
+        totalprice     DECIMAL,
+        orderdate      DATE,
+        orderpriority  CHAR(15),
+        clerk          CHAR(15),
+        shippriority   INT,
+        comment        VARCHAR(79)
+);
+
+CREATE TABLE CUSTOMER (
+        custkey      INT,
+        name         VARCHAR(25),
+        address      VARCHAR(40),
+        nationkey    INT,
+        phone        CHAR(15),
+        acctbal      DECIMAL,
+        mktsegment   CHAR(10),
+        comment      VARCHAR(117)
+);
+
+
+-- SELECT ORDERS.orderkey, 
+--        ORDERS.orderdate,
+--        ORDERS.shippriority,
+--        SUM(extendedprice * (1 - discount)) AS query3
+-- FROM   CUSTOMER, ORDERS, LINEITEM
+-- WHERE  CUSTOMER.mktsegment = 'BUILDING'
+--   AND  ORDERS.custkey = CUSTOMER.custkey
+--   AND  LINEITEM.orderkey = ORDERS.orderkey
+--   AND  ORDERS.orderdate < DATE('1995-03-15')
+--   AND  LINEITEM.shipdate > DATE('1995-03-15')
+-- GROUP BY ORDERS.orderkey, ORDERS.orderdate, ORDERS.shippriority;
+
+
+SELECT LINEITEM.ORDERKEY, SUM(LINEITEM.EXTENDEDPRICE * (1 - LINEITEM.DISCOUNT)) 
+AS REVENUE, ORDERS.ORDERDATE, ORDERS.SHIPPRIORITY FROM CUSTOMER, ORDERS, LINEITEM 
+WHERE CUSTOMER.MKTSEGMENT = 'BUILDING' AND CUSTOMER.CUSTKEY = ORDERS.CUSTKEY AND 
+LINEITEM.ORDERKEY = ORDERS.ORDERKEY 
+AND ORDERS.ORDERDATE < DATE('1995-03-15') AND LINEITEM.SHIPDATE > DATE('1995-03-15') GROUP BY 
+LINEITEM.ORDERKEY, ORDERS.ORDERDATE, ORDERS.SHIPPRIORITY ORDER BY REVENUE DESC, ORDERS.ORDERDATE
